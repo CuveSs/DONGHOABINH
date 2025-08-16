072e4cbf5584b3e2835b5a245e9562de514e8df5
cuong-vu <ducuong1107@gmail.com>
2025-08-16 10:01:21 +0700

    Finish hero page

--- assets/apps/app.js
+++ assets/apps/app.js
@@ -1,5 +1,36 @@
 var imgFeature = document.querySelector(".img-feature");
-var prevBtn = document.querySelector("prev");
-var prevBtn = document.querySelector("next");
+var listImg = document.querySelectorAll(".slideshow-img__list img");
+var prevBtn = document.querySelector(".prev");
+var nextBtn = document.querySelector(".next");
 
-var currentIndex = prevbtn;
+var currentIndex = 0;
+function updateImgByIndex(index) {
+  currentIndex = index;
+  imgFeature.src = listImg[index].getAttribute("src");
+}
+
+listImg.forEach((imgElement, index) => {
+  imgElement.addEventListener("click", (e) => {
+    updateImgByIndex(index);
+  });
+});
+
+prevBtn.addEventListener("click", (e) => {
+  if (currentIndex == 0) {
+    currentIndex = listImg.length - 1;
+  } else {
+    currentIndex--;
+  }
+
+  updateImgByIndex(currentIndex);
+});
+
+nextBtn.addEventListener("click", (e) => {
+  if (currentIndex == listImg.length - 1) {
+    currentIndex = 0;
+  } else {
+    currentIndex++;
+  }
+
+  updateImgByIndex(currentIndex);
+});
